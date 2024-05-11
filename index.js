592c072e0344b252043eda942054b577d2a7d02a
Fazlul Alam <fazlulalam619@gmail.com>
2024-05-11 19:28:50 +0600

    Get Data from mongodb

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const foodCollection = client.db("foodDB").collection("foodCollection");
+
+    app.get("/foodItem", async (req, res) => {
+      const cursor = foodCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.post("/foodItem", async (req, res) => {
       const foodItem = req.body;
       console.log(foodItem);
